be0cceed48d9e7f5253615bc5b82b264dc8fee23
Irina Starodubtseva <st.irina.fedorovna@gmail.com>
2022-08-15 11:31:10 +0300

    changes for my/all articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   post '/auth/login', to: 'authentication#login'
   get '/login', to: 'application#show'
-  get '/all/page/:page', to: 'tasks#users_tasks'
+  get '/all/page/:page', to: 'tasks#all_tasks'
   get '/tasks/page/:page', to: 'tasks#index'
   get '/all/length', to: 'tasks#all_length'
-  get '/tasks/length', to: 'tasks#tasks_length'
+  # get '/tasks/length', to: 'tasks#tasks_length'
   get '/my/page/:page', to: 'tasks#users_tasks_only'
   get '/my/length', to: 'tasks#my_length'
-  post '/new', to: 'tasks#create_globally'
 
 
   resources :tasks
